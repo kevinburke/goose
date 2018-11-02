0bfa0e915d70a49a9190c58c6cfc1a3b4fe14e77
Kevin Burke <kev@inburke.com>
2018-11-01 20:52:14 -1000

    1.10

--- cmd/goose/main.go
+++ cmd/goose/main.go
@@ -96,7 +96,7 @@ func helpRun(*Command, ...string) {
 }
 
 // The version of the goose tool.
-const VERSION = "1.9" // Bump this by running "make release".
+const VERSION = "1.10" // Bump this by running "make release".
 
 func versionRun(*Command, ...string) {
 	fmt.Fprintf(os.Stderr, "goose version %s\n", VERSION)
